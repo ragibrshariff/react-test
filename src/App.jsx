b622cdc0493ebee653aa95f88b0e34466485876f
Ubuntu <ubuntu@ip-172-31-21-92.eu-north-1.compute.internal>
2025-07-13 10:44:34 +0000

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,23 @@ import { useEffect, useState } from "react";
 function App() {
   const [users, setUsers] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("http://localhost:8000/users")
-  //     .then(res => res.json())
-  //     .then(data => setUsers(data));
-  // }, []);
+  useEffect(() => {
+    fetch("http://13.61.145.147:8000/users")
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
 
   return (
     <div style={{ padding: "2rem" }}>
       <h2>Users</h2>
-      {/* <ul>
+      <ul>
         {users.map(u => (
           <li key={u.id}>{u.name}</li>
-        ))} */}
-      {/* </ul> */}
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
