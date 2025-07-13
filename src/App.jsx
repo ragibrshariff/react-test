fa7468553e681ba4d2c2b06f08a4496393cb5916
Ragib <ragibrshariff@gmail.com>
2025-07-13 14:00:54 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:8000/users")
+  //     .then(res => res.json())
+  //     .then(data => setUsers(data));
+  // }, []);
+
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>Users</h2>
+      {/* <ul>
+        {users.map(u => (
+          <li key={u.id}>{u.name}</li>
+        ))} */}
+      {/* </ul> */}
+    </div>
+  );
+}
+
+export default App;
